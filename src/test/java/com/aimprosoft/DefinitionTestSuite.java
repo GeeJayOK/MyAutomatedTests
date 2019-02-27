d2c2cfa098fb9b2eaf0bf54d176e0c63f418e4a8
GeeJayOK <239i086c747q>
2019-02-27 17:30:47 +0200

    38 bugs

--- src/test/java/com/aimprosoft/DefinitionTestSuite.java
+++ src/test/java/com/aimprosoft/DefinitionTestSuite.java
@@ -6,12 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = {
-        //"src/test/resources/features/consult_dictionary/01_RegistrationPage1.feature",
-        //"src/test/resources/features/consult_dictionary/02_RegistrationPage2.feature",
-        //"src/test/resources/features/consult_dictionary/03_AuthorizationPage.feature",
-        //"src/test/resources/features/consult_dictionary/04_PasswordRecoveryPage1.feature",
-        //"src/test/resources/features/consult_dictionary/05_PasswordRecoveryPage2.feature",
-        //"src/test/resources/features/consult_dictionary/06_MainPage.feature",
+        "src/test/resources/features/consult_dictionary/01_RegistrationPage1.feature",
+        "src/test/resources/features/consult_dictionary/02_RegistrationPage2.feature",
+        "src/test/resources/features/consult_dictionary/03_AuthorizationPage.feature",
+        "src/test/resources/features/consult_dictionary/04_PasswordRecoveryPage1.feature",
+        "src/test/resources/features/consult_dictionary/05_PasswordRecoveryPage2.feature",
+        "src/test/resources/features/consult_dictionary/06_MainPage.feature",
         "src/test/resources/features/consult_dictionary/07_UserMenuPage.feature"
 })
 public class DefinitionTestSuite {
